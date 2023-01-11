4ab896a3c18f2f148c4dbe5284e2399fef4b4b9b
Ina Widmer <ina.widmer@unic.com>
2023-01-11 13:38:08 +0100

    set swr cache for all pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   routeRules: {
-    '/about': { static: true },
-    '/posts/**': { swr: 10 }
+    '/**': { swr: 25 }
   }
 })
