03c204548c2157e8ba853e138c131c7229f0da53
Ina Widmer <ina.widmer@unic.com>
2023-01-11 17:09:11 +0100

    Prerender routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/posts/1', '/posts/2', '/posts/3', '/posts/4', '/posts/5']
+    }
+  },
   routeRules: {
     "/about": { static: true },
     "/posts/**": { swr: true },
