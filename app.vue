fa8327e2fc933f99458fcb6aa9f1ab3e566c7809
Ina Widmer <ina.widmer@unic.com>
2023-01-11 16:41:32 +0100

    Add navigation links

--- app.vue
+++ app.vue
@@ -8,7 +8,15 @@
         <li>
           <NuxtLink to="/about">About</NuxtLink>
         </li>
-
+        <li>
+          <NuxtLink to="/posts/1">Article 1</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/2">Article 2</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/3">Article 3</NuxtLink>
+        </li>
       </ul>
     </nav>
     <div><NuxtPage /></div>
