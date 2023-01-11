529453bdb31014eec0b63fa05d7abd622c2196db
Ina Widmer <ina.widmer@unic.com>
2023-01-11 11:35:08 +0100

    remove nitro preset setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'node-server'
-  },
   routeRules: {
     '/about': { static: true },
     '/posts/**': { swr: 10 }
