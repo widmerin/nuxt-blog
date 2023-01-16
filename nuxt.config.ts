4117be44a16349e83fa13e93dba5823d8dab7956
Ina Widmer <ina.widmer@unic.com>
2023-01-16 15:24:59 +0100

    Cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "netlify",
-  },
   routeRules: {
     "/**": { swr: 5 },
-  },
-  hooks: {
-    "nitro:build:before": (ctx) => {
-      const additonalRoutes = Object.keys(ctx.options.routeRules);
-      for (const route of additonalRoutes) {
-        ctx.options.handlers.push({
-          route,
-          handler: ctx.options.renderer,
-          lazy: true,
-          middleware: false,
-          method: undefined,
-        });
-      }
-    },
   },
 });
