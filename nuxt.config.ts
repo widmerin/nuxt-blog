2650f7bff79f29befb2e74f31b27ce1f4bae93f3
Ina Widmer <ina.widmer@unic.com>
2023-01-16 10:28:15 +0100

    set nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    prerender: {
-      routes: ['/','/posts/1', '/posts/2', '/posts/3', '/posts/4', '/posts/5']
-    }
-  },
-  experimental: {
-    payloadExtraction: false,
+    preset: "vercel-edge",
   },
   routeRules: {
-    '/**': { cache: { swr: true, maxAge: 120, staleMaxAge: 60, headersOnly: true } },
+    "/about": { static: true },
+    "/posts/**": { swr: true },
     "/static": { static: true },
     "/spa": { ssr: false },
     "/swr": { swr: true },
   },
   hooks: {
-    'nitro:build:before': (ctx) => {
-      const additonalRoutes = Object.keys(ctx.options.routeRules)
+    "nitro:build:before": (ctx) => {
+      const additonalRoutes = Object.keys(ctx.options.routeRules);
       for (const route of additonalRoutes) {
         ctx.options.handlers.push({
           route,
@@ -25,8 +21,8 @@ export default defineNuxtConfig({
           lazy: true,
           middleware: false,
           method: undefined,
-        })
+        });
       }
-    }
-  }
+    },
+  },
 });
