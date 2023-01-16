9cf757965cb4caf4783b974380c892ac5bcc4166
Ina Widmer <ina.widmer@unic.com>
2023-01-16 16:01:36 +0100

    set payloadExtraction true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
       routes: ["/","/about","/posts/1", "/posts/2", "/posts/3", "/posts/4", "/posts/5"],
     },
   },
+  experimental: {
+     payloadExtraction: true,
+  },
+
   routeRules: {
     "/**": { swr: 10 },
   },
