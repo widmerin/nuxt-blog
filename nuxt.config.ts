d0bf1b8c2314833940eb22692b39adc03680c87f
Ina Widmer <ina.widmer@unic.com>
2023-01-18 13:38:54 +0100

    add payloadExtraction true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  experimental: {
+    payloadExtraction: true,
+  },
+
   routeRules: {
     "/**": { swr: true },
     "/static": { static: true },
