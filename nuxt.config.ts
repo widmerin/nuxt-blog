23df2cad0807427f0bd3d9173020d1c92fbe533d
Ina Widmer <ina.widmer@unic.com>
2023-04-17 15:40:20 +0200

    disable prerendered routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 export default defineNuxtConfig({
-  nitro: {
-    prerender: {
-      routes: [
-        "/",
-        "/test1",
-        "/test2",
-        "/test3",
-        "/posts/1",
-        "/posts/2",
-        "/posts/3",
-      ],
-    },
-  },
+  // nitro: {
+  //   prerender: {
+  //     routes: [
+  //       "/",
+  //       "/test1",
+  //       "/test2",
+  //       "/test3",
+  //       "/posts/1",
+  //       "/posts/2",
+  //       "/posts/3",
+  //     ],
+  //   },
+  // },
   routeRules: {
     "/**": { swr: true },
   },
